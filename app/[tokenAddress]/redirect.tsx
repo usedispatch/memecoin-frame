242161996903639e2025318a80387d2aa7fab174
Zayyan Faizal <zayyan@usedispatch.xyz>
2024-04-02 14:14:51 -0500

    default redirect to solarplex

--- app/[tokenAddress]/redirect.tsx
+++ app/[tokenAddress]/redirect.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { MemecoinFrameCreateForm } from "@/components/MemecoinFrameCreateForm"
 import { redirect } from "next/navigation"
 import { useEffect } from "react"
 
@@ -7,7 +8,11 @@ import { useEffect } from "react"
 export default function SolarplexRedirect(props: {did: string, post: string}) {
     const { did, post } = props
     useEffect(() => {
-        redirect(`https://solarplex.xyz/profile/${did}/post/${post}`)
+        if (did && post) {
+            redirect(`https://solarplex.xyz/profile/${did}/post/${post}`)
+        } else {
+            redirect(`https://solarplex.xyz/`)
+        }
     }, [did, post])
   
     return <></>
