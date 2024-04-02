c676b6fae42666de4745a727fe942d1dad85c41f
Zayyan Faizal <zayyan@usedispatch.xyz>
2024-04-02 10:51:31 -0500

    restore config

--- config.ts
+++ config.ts
@@ -1,3 +1,2 @@
-// export const NEXT_PUBLIC_URL = 'https://memecoin-frame.vercel.app';
+export const NEXT_PUBLIC_URL = 'https://memecoin-frame.vercel.app';
 // export const NEXT_PUBLIC_URL= 'http://localhost:9001'
-export const NEXT_PUBLIC_URL = 'https://memecoin-frame-git-personalizedframe-usedispatch.vercel.app';
