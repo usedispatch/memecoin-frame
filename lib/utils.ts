95f579ca1f843ddb53364e00497dfa6f890eef93
Zayyan Faizal <zayyan@usedispatch.xyz>
2024-04-02 17:05:12 -0500

    encode number

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,24 +6,25 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function encryptNumberWithKey(number: string, key: string): string {
-  let encryptedNumber = '';
-  for (let i = 0; i < number.length; i++) {
-      let charCode = number.charCodeAt(i);
-      let keyChar = key.charCodeAt(i % key.length);
-      let encryptedCharCode = charCode + keyChar;
-      encryptedNumber += String.fromCharCode(encryptedCharCode);
-  }
-  return btoa(encryptedNumber); // Base64 encode
+  // let encryptedNumber = '';
+  // for (let i = 0; i < number.length; i++) {
+  //     let charCode = number.charCodeAt(i);
+  //     let keyChar = key.charCodeAt(i % key.length);
+  //     let encryptedCharCode = charCode + keyChar;
+  //     encryptedNumber += String.fromCharCode(encryptedCharCode);
+  // }
+  return btoa(number); // Base64 encode
 }
 
 export function decryptNumberWithKey(encryptedNumber: string, key: string) {
   let decryptedNumber = '';
   encryptedNumber = atob(encryptedNumber); // Base64 decode
-  for (let i = 0; i < encryptedNumber.length; i++) {
-      let charCode = encryptedNumber.charCodeAt(i);
-      let keyChar = key.charCodeAt(i % key.length);
-      let decryptedCharCode = charCode - keyChar;
-      decryptedNumber += String.fromCharCode(decryptedCharCode);
-  }
+  decryptedNumber = encryptedNumber;
+  // for (let i = 0; i < encryptedNumber.length; i++) {
+  //     let charCode = encryptedNumber.charCodeAt(i);
+  //     let keyChar = key.charCodeAt(i % key.length);
+  //     let decryptedCharCode = charCode - keyChar;
+  //     decryptedNumber += String.fromCharCode(decryptedCharCode);
+  // }
   return decryptedNumber;
 }
